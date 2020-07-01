cbb7b5d9c8bc42b6da6dde9c7e22e1749c2916cb
aGuyNamedJonas <hi@aGuyNamedJonas.com>
2020-07-01 12:35:56 +0200

    Delete obsolete tools

--- tools/npm-publisher-probot/src/index.ts
+++
@@ -1,8 +0,0 @@
-import { Application } from "probot";
-
-export = (app: Application) => {
-  app.log("Yay, the app was loaded!");
-
-  const handlePullRequest = require("./pullRequestChange");
-  app.on(["pull_request"], handlePullRequest );
-};
