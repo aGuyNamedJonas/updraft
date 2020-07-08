1691776cbf6e201bdefb3da0d456dc8dc8609fd2
aGuyNamedJonas <hi@aGuyNamedJonas.com>
2020-07-08 12:57:29 +0200

    Add env option

--- modules/typescript/aws-lambdas-multi-handler/index.ts
+++ modules/typescript/aws-lambdas-multi-handler/index.ts
@@ -2,6 +2,7 @@
 import { Construct, Duration } from '@aws-cdk/core'
 import * as Lambda from '@aws-cdk/aws-lambda'
 
+type Env = { key: string, value: string }
 export type LambdasMultiHandlerProps = {
   handlerNames: string[]
   localCodePath: string
@@ -9,6 +10,9 @@ export type LambdasMultiHandlerProps = {
   // Defaults to Node.js v12 - just because that's a personal favorite 🤓
   runtime?: Lambda.Runtime
   rootExport?: string
+  env?: {
+    [handlerName: string]: Env[]
+  }
 }
 
 export class AwsLambdasMultiHandler extends Construct {
@@ -20,15 +24,23 @@ export class AwsLambdasMultiHandler extends Construct {
     timeoutSec = 3,
     runtime = Lambda.Runtime.NODEJS_12_X,
     rootExport = 'index',
+    env = {}
   }: LambdasMultiHandlerProps) {
     super(parent, name)
     this.handlers = {}
     handlerNames.forEach(
-      handlerName => this.createLambda(handlerName, runtime, timeoutSec, rootExport, localCodePath)
+      handlerName => this.createLambda(
+        handlerName,
+        runtime,
+        timeoutSec,
+        rootExport,
+        localCodePath,
+        env[handlerName] ? env[handlerName] : []
+      )
     )
   }
 
-  createLambda (handlerName: string, runtime: Lambda.Runtime, timeoutSec: number, rootExport: string, localCodePath: string) {
+  createLambda (handlerName: string, runtime: Lambda.Runtime, timeoutSec: number, rootExport: string, localCodePath: string, env: Env[]) {
     const handler = new Lambda.Function(this, handlerName, {
       runtime,
       timeout: Duration.seconds(timeoutSec),
@@ -37,6 +49,10 @@ export class AwsLambdasMultiHandler extends Construct {
       // This enforces updates on redeploy
       description: `Version: ${new Date().toISOString()}`
     })
+    env.forEach(({ key, value }) => {
+      handler.addEnvironment(key, value)
+    })
+
     this.handlers[handlerName] = handler
   }
 }
