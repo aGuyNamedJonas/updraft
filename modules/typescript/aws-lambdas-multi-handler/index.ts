8041afcfb69392143646ef1522648446cbb66fef
aGuyNamedJonas <hi@aGuyNamedJonas.com>
2020-07-09 12:42:43 +0200

    Add bucket-source for lambda functions

--- modules/typescript/aws-lambdas-multi-handler/index.ts
+++ modules/typescript/aws-lambdas-multi-handler/index.ts
@@ -1,11 +1,16 @@
 #!/usr/bin/env node
 import { Construct, Duration } from '@aws-cdk/core'
 import * as Lambda from '@aws-cdk/aws-lambda'
+import * as S3 from '@aws-cdk/aws-s3'
 
 type Env = { key: string, value: string }
 export type LambdasMultiHandlerProps = {
   handlerNames: string[]
-  localCodePath: string
+  localCodePath?: string
+  codeBucket?: {
+    bucketName: string
+    key: string
+  }
   timeoutSec?: number
   // Defaults to Node.js v12 - just because that's a personal favorite 🤓
   runtime?: Lambda.Runtime
@@ -20,7 +25,11 @@ export class AwsLambdasMultiHandler extends Construct {
 
   constructor(parent: Construct, name: string, {
     handlerNames,
-    localCodePath,
+    localCodePath = '',
+    codeBucket = {
+      bucketName: 'not_set',
+      key: 'not_set',
+    },
     timeoutSec = 3,
     runtime = Lambda.Runtime.NODEJS_12_X,
     rootExport = 'index',
@@ -35,17 +44,31 @@ export class AwsLambdasMultiHandler extends Construct {
         timeoutSec,
         rootExport,
         localCodePath,
+        codeBucket,
         env[handlerName] ? env[handlerName] : []
       )
     )
   }
 
-  createLambda (handlerName: string, runtime: Lambda.Runtime, timeoutSec: number, rootExport: string, localCodePath: string, env: Env[]) {
+  createLambda (
+    handlerName: string,
+    runtime: Lambda.Runtime,
+    timeoutSec: number,
+    rootExport: string,
+    localCodePath: string,
+    codeBucket: {
+      bucketName: string
+      key: string
+    },
+    env: Env[]
+  ) {
     const handler = new Lambda.Function(this, handlerName, {
       runtime,
       timeout: Duration.seconds(timeoutSec),
       handler: `${rootExport}.${handlerName}`,
-      code: Lambda.Code.asset(localCodePath),
+      code: localCodePath
+            ? Lambda.Code.fromAsset(localCodePath)
+            : Lambda.Code.fromBucket(S3.Bucket.fromBucketName(this, 'codeBucket', codeBucket.bucketName), codeBucket.key),
       // This enforces updates on redeploy
       description: `Version: ${new Date().toISOString()}`
     })
