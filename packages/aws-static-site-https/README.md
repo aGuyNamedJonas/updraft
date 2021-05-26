404bb04940d9df4660028753fce1d361775c2370
aGuyNamedJonas <hi@aGuyNamedJonas.com>
2021-05-26 07:43:11 +0300

    Add v0.0.1 aws-static-site-https

---
+++ packages/aws-static-site-https/README.md
@@ -0,0 +1,6 @@
+# aws-static-site-https
+
+
+# Install
+1. If you want to use lambda-edge functions, make sure to bootstrap `us-east-1` for AWS CDK usage. (e.g. by running `npx cdk bootstrap aws://<AWS-ACCOUNT-ID>/us-east-1`)
+2. `yarn add @updraft/aws-static-site-https`
---
+++ packages/aws-static-site-https/lambda-edge/subfolders.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// Inspired by:
+// https://hackernoon.com/how-to-host-a-single-page-application-with-aws-cloudfront-and-lambda-edge-39ce7b036da2
+exports.handler = (event, context, callback) => {
+    const {request} = event.Records[0].cf
+
+    if (!path.extname(request.uri)) {
+        request.uri += '/index.html'
+    }
+
+    callback(null, request)
+};
